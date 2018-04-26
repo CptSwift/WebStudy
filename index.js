9e8e59f0baee4975fabcea85a9adbc9db3a17a88
zhangzihao <379882792@qq.com>
2018-04-26 23:21:49 +0800

    ADD : master : #comment 使用webpack构建项目并显示hello world

---
+++ index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by chenz on 2018/4/26.
+ */
+document.write("hello world");
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+/**
+ * Created by chenz on 2018/4/26.
+ */
+let path = require('path');
+let webpack = require('webpack');
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.join(__dirname, 'dist'), //输出目录的配置，模板、样式、脚本、图片等资源的路径配置都相对于它
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+
+        ]
+    }
+};
